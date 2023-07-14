6b0d4d03687419bcc465d01f6d36da25e20c5151
mike0295 <m.seo1999@gmail.com>
2023-07-14 11:47:00 +0900

    remove unused template code

--- src/App.js
+++ src/App.js
@@ -35,23 +35,8 @@ import { PaperSdkContextProvider } from '@contexts/paperSdkContext';
 
 // pages
 const Home = lazy(() => import('@pages/Home'));
-const Home2 = lazy(() => import('@pages/Home2'));
 const Collection = lazy(() => import('@pages/Collection'));
 const ExploreGrid = lazy(() => import('@pages/ExploreGrid'));
-const Item = lazy(() => import('@pages/Item'));
-const Author = lazy(() => import('@pages/Author'));
-const Profile = lazy(() => import('@pages/Profile'));
-const FAQ = lazy(() => import('@pages/FAQ'));
-const Ranking = lazy(() => import('@pages/Ranking'));
-const Activity = lazy(() => import('@pages/Activity'));
-const ConnectWallet = lazy(() => import('@pages/ConnectWallet'));
-const Login = lazy(() => import('@pages/Login'));
-const About = lazy(() => import('@pages/About'));
-const Team = lazy(() => import('@pages/Team'));
-const BlogSidebar = lazy(() => import('@pages/BlogSidebar'));
-const BlogGrid = lazy(() => import('@pages/BlogGrid'));
-const Post = lazy(() => import('@pages/Post'));
-const Contacts = lazy(() => import('@pages/Contacts'));
 const PageNotFound = lazy(() => import('@pages/PageNotFound'));
 
 const { provider, webSocketProvider } = configureChains(
@@ -103,23 +88,8 @@ const App = () => {
                                         <Suspense fallback={<LoadingScreen visible />}>
                                             <Routes>
                                                 <Route path="/" element={<Home />} />
-                                                {/* <Route path="/index2" element={<Home2 />} /> */}
                                                 <Route path="/collection/:id" element={<Collection />} />
                                                 <Route path="/search/:query" element={<ExploreGrid />} />
-                                                <Route path="/explore/token/:id" element={<Item />} />
-                                                {/* <Route path="/author" element={<Author />} /> */}
-                                                {/* <Route path="/profile" element={<Profile />} /> */}
-                                                <Route path="/faq" element={<FAQ />} />
-                                                <Route path="/ranking" element={<Ranking />} />
-                                                <Route path="/activity" element={<Activity />} />
-                                                {/* <Route path="/connect-wallet" element={<ConnectWallet />} /> */}
-                                                {/* <Route path="/login" element={<Login />} /> */}
-                                                {/* <Route path="/about" element={<About />} /> */}
-                                                {/* <Route path="/team" element={<Team />} /> */}
-                                                {/* <Route path="/blog-sidebar" element={<BlogSidebar />} /> */}
-                                                {/* <Route path="/blog-grid" element={<BlogGrid />} /> */}
-                                                {/* <Route path="/post" element={<Post />} /> */}
-                                                {/* <Route path="/contacts" element={<Contacts />} /> */}
                                                 <Route path="*" element={<PageNotFound />} />
                                             </Routes>
                                         </Suspense>
